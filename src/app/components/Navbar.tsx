a01b3f76487466c76b649175b9ccb5f5b3c946ec
Antonio <toony1908@gmail.com>
2022-08-21 03:46:43 -0500

    Add wallets

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import stitchLogo from "assets/stitchLogo.svg";
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from "./Button";
 import { useLocation, useNavigate } from "react-router-dom";
 import magnifyingGlass from "assets/magnifyingGlass.svg";
@@ -7,49 +7,22 @@ import WalletConnect from "@walletconnect/client";
 import QRCodeModal from "@walletconnect/qrcode-modal";
 import { useWeb3React } from "@web3-react/core";
 
-import PlatziPunksArtifact from "../../../src/config/web3/artifacts/stitch";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-
-//import WalletConnectProvider from "@maticnetwork/walletconnect-provider"
-
-
-
-import Web3 from "web3"
-//import Matic from "maticjs"
-
-import { connector } from "../../config/web3";
-
-const { address, abi } = PlatziPunksArtifact;
-
 interface INavbar {
   children: JSX.Element;
 }
 
 const Navbar = ({ children }: INavbar) => {
   const [conn, setConn] = useState(null);
-  /*const { active, library, chainId } = useWeb3React();*/
+  const { active, library, chainId } = useWeb3React();
 
-  /*let  connect = async () => {
-    // bridge url
+  let  connect = async () => {
     const bridge = "https://bridge.walletconnect.org";
-
-    // create new connector
     const connector = new WalletConnect({ bridge, qrcodeModal: QRCodeModal });
 
-    //await this.setState({ connector });
+    connector.killSession()
 
-    //connector.killSession()
-
-    // check if already connected
     if (!connector.connected) {
-      // create new session
-      console.log("connectionx")
       connector.createSession();
-
-      console.log(chainId)
-      console.log(connector.chainId)
-
-      console.log(active)
     }
 
     connector.on("connect", (error, payload) => {
@@ -57,56 +30,11 @@ const Navbar = ({ children }: INavbar) => {
         throw error;
       }
 
-      // Get provided accounts and chainId
-      const { accounts, chainId } = payload.params[0];
-
-      console.log("accounts")
-      console.log(accounts)
-
-      /!*new library.eth.Contract(abi, "0x8eD00A727EA01a09A48CEcD779DCd3a07130A13a");*!/
-
-      const tx = {
-        from: accounts[0],
-        to: "0x57880D5B70890446eac85C4d852AA585b67B7134",
-        gas: 800000,
-      }
-
-      console.log("tx")
-      console.log(tx)
-
-// Send transaction
-      connector
-          .sendTransaction(tx)
-          .then((result) => {
-            console.log("result")
-            // Returns transaction id (hash)
-            console.log(result);
-          })
-          .catch((error) => {
-            console.log("error")
-            // Error returned when rejected
-            console.error(error);
-          });
-
-
       setConn(conn)
     });
 
     // subscribe to events
     //await subscribeToEvents();
-  };*/
-
-  const { active, activate, deactivate, account, error, library } =
-      useWeb3React();
-
-  const connect = useCallback(() => {
-    activate(connector);
-    localStorage.setItem("previouslyConnected", "true");
-  }, [activate]);
-
-  const disconnect = () => {
-    deactivate();
-    localStorage.removeItem("previouslyConnected");
   };
 
   const location = useLocation();
